d0324a574684ee35c198d9051dd03ae478b06bf8
Emir Balic <emirbalic@gmail.com>
2018-11-24 10:45:29 +0100

    first version wp2

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+const ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+const config = {
+
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js' 
+    },
+    module: {
+        rules: [
+            {
+                use: 'babel-loader',
+                test: /\.js$/
+            },
+            {
+                loader: ExtractTextPlugin.extract({
+                    loader: 'css-loader'
+                }), 
+                test: /\.css$/
+            }
+        ]
+    }, 
+    plugin: [
+        new ExtractTextPlugin('style.css')
+    ]
+        
+};
+
+module.exports = config;
